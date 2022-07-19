cd11131ed55c8f052d170430d4f5b0e7090fb770
zachglick <glickzachary@gmail.com>
2022-07-19 16:38:11 -0400

    meeting_1

---
+++ meeting_1/run_linear_regression.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LinearRegression
+
+if __name__ == "__main__":
+
+    df = pd.read_csv("QM9.csv")
+    
+    X = df[["n_H", "n_C", "n_N", "n_O", "n_F"]].to_numpy()
+    y = df[["E (Hartree)"]].to_numpy()
+    model = LinearRegression(fit_intercept=True)
+    
+    print(X.shape, y.shape)
